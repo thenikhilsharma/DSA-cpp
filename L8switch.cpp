450aa807465b00d5a954c2320515c61178d09a52
Nikhil Sharma <83294513+thenikhilsharma@users.noreply.github.com>
2025-05-28 02:20:40 +0530

    switch

---
+++ L8switch.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n = 2;
+  char c = '1';
+  int d = 0;
+
+  switch (d)
+  {
+  case '1':
+    cout << "thiis is case 1" << endl;
+    break;
+
+  case 2:
+    cout << "this is case 2" << endl;
+    break;
+
+  default:
+    cout << "this is default" << endl;
+  }
+}
