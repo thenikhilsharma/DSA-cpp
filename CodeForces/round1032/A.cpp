276c9e24d5344e997485112ef7d9000ba72430fe
Nikhil Sharma <83294513+thenikhilsharma@users.noreply.github.com>
2025-06-30 19:27:44 +0530

    codeforces

---
+++ CodeForces/round1032/A.cpp
@@ -0,0 +1,25 @@
+// Letter Home
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution() {
+  public:
+    int move() {}
+};
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, s;
+    cin >> n >> s;
+    vector<int> arr(n);
+    for (int i=0; i<n; i++) {
+      cin >> arr[i];
+    }
+
+    Solution sol;
+  }
+}
