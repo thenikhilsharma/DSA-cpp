c1ca5cdd4914c4f711af4bf418e815cb6a9078e1
Nikhil Sharma <83294513+thenikhilsharma@users.noreply.github.com>
2025-07-01 13:57:23 +0530

    cf

--- CodeForces/round1032/A.cpp
+++ CodeForces/round1032/A.cpp
@@ -1,12 +1,24 @@
-// Letter Home
+// A. Letter Home
+// a = {1, 2, 3, 9, 10, 11}
+// s = 6
+// (s- a[0])*2 + (a[n-1] - s)
 
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-class Solution() {
+class Solution {
   public:
-    int move() {}
+    void f(const vector<int>& a, int n, int s) {
+      if (n == 0) cout << 0 << endl;
+      int ans = 0;
+      if (s <= a[0]) ans = a[n-1] - s;
+      else if (s >= a[n-1]) ans = s - a[0];
+      else ans = (a[n-1] - a[0] + min(s-a[0], a[n-1]-s));
+      cout << ans << endl;
+      return;
+    }
 };
 
 int main() {
@@ -21,5 +33,6 @@ int main() {
     }
 
     Solution sol;
+    sol.f(arr, n, s);
   }
 }
