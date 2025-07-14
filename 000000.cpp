badcdc9a17042c8e1778cdd015fb5ba0df084298
TheNikhilSharma <nikhilsharmalku@gmail.com>
2025-07-15 02:44:26 +0530

    temp

--- 000000.cpp
+++ 000000.cpp
@@ -1,19 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void selectionSort(vector<int> &arr, int idx = 0){
-  if (idx == static_cast<int>(arr.size())) return;
-  int min_index = min_element(arr.begin() + idx, arr.end()) - arr.begin();
-  swap(arr[idx], arr[min_index]);
-  selectionSort(arr, idx+1);
-}
-
 int main() {
+	// your code goes here
 
-  vector<int> arr = {13, 46, 24, 52, 20, 9};
-  selectionSort(arr);
-  for (auto i: arr) cout << i << " ";
-  cout << endl;
-
-  return 0;
 }
