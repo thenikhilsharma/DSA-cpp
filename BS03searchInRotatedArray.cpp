28cd4565630f32af212ae57bd475cd3822029d29
TheNikhilSharma <nikhilsharmalku@gmail.com>
2026-07-24 04:14:53 +0530

    binary search

---
+++ BS03searchInRotatedArray.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+  int search(vector<int>& nums, int target) {
+    int st = 0, end = nums.size() - 1;
+        
+    while (st <= end) {
+      int mid = st + (end - st) / 2;
+      if (nums[mid] == target) return mid;
+
+      // Left half is sorted
+      if (nums[st] <= nums[mid]) {
+        if (nums[st] <= target && target < nums[mid]) {
+          end = mid - 1;
+        } else {
+          st = mid + 1;
+        }
+      } 
+      // Right half is sorted
+      else {
+        if (nums[mid] < target && target <= nums[end]) {
+          st = mid + 1;
+        } else {
+          end = mid - 1;
+        }
+      }
+    }
+  return -1;  // Target not found
+  }
+};
