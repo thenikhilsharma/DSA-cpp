9b85024951da094c523a02fa54e17713385c6603
TheNikhilSharma <nikhilsharmalku@gmail.com>
2026-07-27 04:59:38 +0530

    test file

--- 000000.cpp
+++ 000000.cpp
@@ -1,15 +1,3 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-	vector<int> vec = {'a', 'b', 'c', 'd', 'e'};
-	int count = vec.count('c');
-	int target = 'c';
-	int find = target + 1;
-	int i = 0;
-	while (find < vec[i]) {
-		i++;
-	}
-	cout << vec[i];
-	return 0;
-}
