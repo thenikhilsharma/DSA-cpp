8623a1578c32bdf372cbc99b690f483e41f4ea5d
TheNikhilSharma <nikhilsharmalku@gmail.com>
2026-08-25 20:24:57 +0530

    test files

--- 000000.cpp
+++ 000000.cpp
@@ -2,8 +2,5 @@
 using namespace std;
 
 int main() {
-  vector<int> v = {1, 2, 3, 4, 5};
-  sort(v.begin(), v.end(), [](int a, int b) {
-    return a > b; // Sort in descending order
-  });
+  return 0;
 }
