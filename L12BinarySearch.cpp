48d36cbd980d81f8b972d83bac073cbd5f94727c
TheNikhilSharma <nikhilsharmalku@gmail.com>
2026-09-02 03:33:42 +0530

    Update L12BinarySearch.cpp

--- L12BinarySearch.cpp
+++ L12BinarySearch.cpp
@@ -7,7 +7,7 @@ int binarySearch(int arr[], int size, int key){
 
   int start = 0;
   int end = size - 1;
-  
+
   while (start <= end) {
     int mid = start + (end - start) / 2;
     if (arr[mid] < key) start = mid+1;
